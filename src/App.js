7252ee45504d9b2188c7b69959fcc040da979130
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-11 09:41:02 +0100

    set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react'
 
+const getStyle = color => ({
+  border: `2px solid ${color}`,
+  padding: '10px',
+  margin: '15px',
+})
+
 class App extends Component {
-  // Create a slice of state called "on"
-  // This slice can be either true or false
-  // The initial value should be false
+  constructor(props) {
+    super(props)
+    this.state = {
+      on: false,
+      count: 0,
+    }
+  }
+
+  onTurnOff = evt => {
+
+  }
+
+  onTurnOn = evt => {
+
+  }
+
+  onIncrement = evt => {
+
+  }
 
   render() {
     return (
-      <div className="app">
-        The subcomponent is OFF
+      <div className="app" style={getStyle('black')}>
+        <h4>App component</h4>
+        {/* Make the OFF/ON label be state-driven */}
+        <div>Subcomponents are OFF</div>
+
+        <div className="buttons">
+          {/* Make the buttons work */}
+          <button>turn ON</button>
+          <button>turn OFF</button>
+        </div>
+
+        {/* Counter should only exist if this.state.on === true */}
+        {/* Counter needs data and callbacks to function */}
+        <Counter />
+      </div>
+    )
+  }
+}
+
+class Counter extends Component {
+  // Make this component work
+  render() {
+    return (
+      <div className="counter" style={getStyle('green')}>
+        <h4>Counter Subcomponent</h4>
+        <div>The count is 0</div>
+        <button>Increment</button>
       </div>
     )
   }
